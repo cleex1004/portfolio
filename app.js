cd94443608b23d0415e798fc90c7cfba9080b437
cleex1004 <cleex1004@gmail.com>
2017-02-08 11:35:52 -0800

    hidden nav, proper viewport, constructor, wireframe approved

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-function Project(name, date, description) {
+function Project(name, date, description, image) {
   this.name = name;
   this.date = date;
   this.description = description;
+  this.image = image;
 }
 
 var busmall = new Project('Busmall', 'January 2017', '201 week 3 project');
