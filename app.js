503e8f1150f9f8ab09f1c3293c5821b2c65793af
cleex1004 <cleex1004@gmail.com>
2017-02-09 13:04:10 -0800

    project array jquery

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 'use strict';
 
-function Project(name, date, description, image, link) {
-  this.name = name;
-  this.date = date;
-  this.description = description;
-  this.image = image;
-  this.link = link;
+var projects = [];
+
+function Project(opts) {
+  this.name = opts.name;
+  this.date = opts.date;
+  this.description = opts.description;
+  this.image = opts.image;
+  this.link = opts.link;
 }
 
-var busmall = new Project('Busmall', 'January 2017', '201 week 3 project');
+Project.prototype.toHtml = function() {
+  var $newProject = $('section.project-template').clone();
+  $newProject.find('div').removeClass('hidden');
+  $newProject.find('h4.project-name').html(this.name);
+  $newProject.find('h4.project-date').html(this.date);
+  $newProject.find('p').html(this.description);
+  $newProject.find('img').attr('src', this.image);
+  $newProject.find('a').attr('href', this.link);
+  return $newProject;
+}
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(a) {
+  console.log(a);
+  $('#projects').append(a.toHtml());
+});
---
+++ projects.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var projectData = [
+  {
+    name: 'Busmall',
+    date: '2017-1-16',
+    description: '201 week 3 project',
+    image: 'https://static01.nyt.com/images/2009/04/30/fashion/30skymall.3-190.jpg',
+    link: 'https://cleex1004.github.io/bus-mall/',
+  },
+  {
+    name: 'Salmon Cookies',
+    date: '2017-1-9',
+    description: '201 week 2 project',
+    image: 'http://cookiecritters.com/yahoo_site_admin/assets/images/Salmon.108234858_std.jpg',
+    link: 'https://cleex1004.github.io/cookie-stand/',
+  },
+  {
+    name: 'Meme-ory',
+    date: '2017-1-23',
+    description: '201 week 4 group project',
+    image: 'http://i.imgur.com/v9bw6xj.png',
+    link: 'https://the-meme-team.github.io/meme-ory/',
+  },
+]
