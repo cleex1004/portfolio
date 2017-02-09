25288719373d9ca2f30302901d1a3b88dd3b09f9
cleex1004 <cleex1004@gmail.com>
2017-02-09 11:24:05 -0800

    social media and template added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-function Project(name, date, description, image) {
+function Project(name, date, description, image, link) {
   this.name = name;
   this.date = date;
   this.description = description;
   this.image = image;
+  this.link = link;
 }
 
 var busmall = new Project('Busmall', 'January 2017', '201 week 3 project');
