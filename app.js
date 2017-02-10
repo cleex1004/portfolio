80fe1e4236b88fb70b6366e081d7be33e1e769d1
cleex1004 <cleex1004@gmail.com>
2017-02-09 16:17:35 -0800

    it doesnt work it wont append the 2nd and 3rd

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ function Project(opts) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('section.project-template').clone();
-  $newProject.find('div').removeClass('hidden');
+  var $newProject = $('article.project-template');
+  $newProject.removeClass('project-template');
   $newProject.find('h4.project-name').html(this.name);
   $newProject.find('h4.project-date').html(this.date);
   $newProject.find('p').html(this.description);
   $newProject.find('img').attr('src', this.image);
   $newProject.find('a').attr('href', this.link);
   return $newProject;
-}
+};
 
 projectData.forEach(function(projectObject) {
   projects.push(new Project(projectObject));
-});
+})
 
 projects.forEach(function(a) {
   console.log(a);
   $('#projects').append(a.toHtml());
-});
+})
