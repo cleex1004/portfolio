a3495f12b591cb139f5d0f5f63a9a7c7f41e1a57
cleex1004 <cleex1004@gmail.com>
2017-02-21 12:06:40 -0800

    added IIFEs .map .reduce facts

--- public/scripts/projectView.js
+++ public/scripts/projectView.js
@@ -1,45 +1,52 @@
 'use strict';
 
-const projectView = {};
+(function(module){
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-  $('.main-nav .tab:first').click();
-};
+  const projectView = {};
 
-projectView.initIndexPage = function() {
-  Project.all.forEach(function(a) {
-    $('#projects').append(a.toHtml())
-  });
-  projectView.handleMainNav();
-};
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      $('.tab-content').hide();
+      $('#' + $(this).data('content')).fadeIn();
+    });
+    $('.main-nav .tab:first').click();
+  };
 
-projectView.create = function() {
-  let project;
-  $('#projects').empty();
+  projectView.initIndexPage = function() {
+    Project.all.forEach(function(a) {
+      $('#projects').append(a.toHtml())
+    });
+    projectView.handleMainNav();
+    $('#facts .days').text(Project.days());
+    $('#facts .number').text(Project.all.length);
+  };
 
-  project = new Project({
-    name: $('#project-name').val(),
-    date: $('#project-date').val(),
-    description: $('#project-description').val(),
-    image: $('#project-image').val(),
-    link: $('#project-link').val(),
-  });
-  $('#projects').append(project.toHtml());
+  projectView.create = function() {
+    let project;
+    $('#projects').empty();
 
-  $('#export-field').show();
-  $('#project-json').val(`${JSON.stringify(project)},`);
-};
+    project = new Project({
+      name: $('#project-name').val(),
+      date: $('#project-date').val(),
+      description: $('#project-description').val(),
+      image: $('#project-image').val(),
+      link: $('#project-link').val(),
+    });
+    $('#projects').append(project.toHtml());
 
-projectView.initNewProjectPage = function() {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#project-json').on('focus', function(){
-    this.select();
-  });
+    $('#export-field').show();
+    $('#project-json').val(`${JSON.stringify(project)},`);
+  };
 
-  $('#new-form').on('change', 'input, textarea', projectView.create);
-};
+  projectView.initNewProjectPage = function() {
+    $('.tab-content').show();
+    $('#export-field').hide();
+    $('#project-json').on('focus', function(){
+      this.select();
+    });
+
+    $('#new-form').on('change', 'input, textarea', projectView.create);
+  };
+
+  module.projectView = projectView;
+})(window);
